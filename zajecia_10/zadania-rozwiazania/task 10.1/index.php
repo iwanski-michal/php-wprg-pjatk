01122ca8de9efdfc0363442988a46dbd4f68f8bb
OGNIOMAN <ognioman@gmail.com>
2022-05-19 16:31:07 +0200

    task 10

--- zajecia_10/zadania-rozwiazania/task 10.1/index.php
+++ zajecia_10/zadania-rozwiazania/task 10.1/index.php
@@ -1,22 +1,22 @@
 <?php
 $dbuser = 'root';
 $dbpass = '';
-$db = new PDO("mysql:host=localhost;dbname=", $dbuser,$dbpass) or die ("Wywaliłem sie");
-if(isset($_GET['mainId'])){
-    $mainId=$_GET['mainId'];
+$db = new PDO("mysql:host=localhost;dbname=cars", $dbuser,$dbpass) or die ("Błąd inicjalizaji bazy :C");
+if(isset($_GET['itemId'])){
+    $itemId=$_GET['itemId'];
 }
 else{
-    $mainId=1;
+    $itemId=1;
 }
-if($mainId>1){
-    $prev=$mainId-1;
+if($itemId>1){
+    $prev=$itemId-1;
 }
 else{
     $prev = 1;
 }
-$idMax = $mainId*5;
+$idMax = $itemId*5;
 $idMin = $idMax-4;
-$next=$mainId+1;
+$next=$itemId+1;
 
 ?>
 <!DOCTYPE html>
@@ -26,11 +26,28 @@ $next=$mainId+1;
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Auta</title>
     <style>
-        table{
-            border-collapse: collapse;
+                body {
+            text-align: center;
         }
-        td{
-            border: 3px solid whitesmoke;
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  text-align:center;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+tr:hover {
+          background-color: #ababab;
         }
     </style>
 </head>
@@ -49,12 +66,12 @@ foreach ($db->query($sql) as $row) {
     echo "<td>".$row['id'] . "</td>";
     echo "<td>".$row['marka'] . "</td>";
     echo "<td>".$row['model'] . "</td>";
-    echo '<td><a href="carInfo.php?id=' . $row['id'] . '">Czytaj dalej</a> <br></td>';
+    echo '<td><a href="additionalInfo.php?id=' . $row['id'] . '">Czytaj dalej</a> <br></td>';
     echo '</tr>';
 }
 echo '</table>';
-echo '<button><a href="index.php?mainId='.$prev.'">Poprzednie</a></button>';
-echo '<button><a href="index.php?mainId='.$next.'">Następne</a></button>';
+echo '<button><a href="index.php?itemId='.$prev.'">Poprzednie</a></button>';
+echo '<button><a href="index.php?itemId='.$next.'">Następne</a></button>';
 ?>
 
 </body>
